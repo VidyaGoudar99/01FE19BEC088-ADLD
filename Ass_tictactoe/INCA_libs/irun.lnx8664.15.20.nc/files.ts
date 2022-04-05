8349868c65166d93fce99408fb3a529fa610f74e
VidyaGoudar99 <59998507+VidyaGoudar99@users.noreply.github.com>
2022-04-05 21:27:49 +0530

    Add files via upload

---
+++ Ass_tictactoe/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,6 @@
+1649068563 /home/adld25/01fe19bec096/Ass_tictactoe/cds.lib
+1649068544 /home/adld25/01fe19bec096/Ass_tictactoe/hdl.var
+1638271895 /home/adld25/cds.lib
+1638271907 /home/adld25/hdl.var
+1649069500 /home/adld25/01fe19bec096/Ass_tictactoe/tic_tb.v
+1649068357 /home/adld25/01fe19bec096/Ass_tictactoe/tic.v
